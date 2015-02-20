cd6547d00178a76d3f3d9b9f927d690fee835e76
Jens Hedegaard Nielsen <jens.nielsen@ucl.ac.uk>
2015-02-20 11:14:20 +0000

    Remove stuff giving problems that we will not need

--- setup/swc-installation-test-2.py
+++ setup/swc-installation-test-2.py
@@ -70,11 +70,11 @@ CHECKS = [
     'virtual-browser',
 # Version control
     'git',
-    'hg',              # Command line tool
+    #'hg',              # Command line tool
     #'mercurial',       # Python package
-    'EasyMercurial',
+    #'EasyMercurial',
 # Build tools and packaging
-    'make',
+    #'make',
     'virtual-pypi-installer',
     'setuptools',
     #'xcode',
@@ -98,7 +98,7 @@ CHECKS = [
     'sympy',
     'Cython',
     'networkx',
-    'mayavi.mlab',
+    #'mayavi.mlab',
     ]
 
 CHECKER = {}
